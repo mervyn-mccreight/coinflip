1d2b98415ae1da4c745d16e300c2dbbc4ca3762b
Mervyn McCreight <mervyn.mccreight@gmail.com>
2015-11-29 13:57:43 +0100

    introduce specification for step 7 in protocol and parsing test.

--- src/main/java/de/fhwedel/coinflip/protocol/model/id/ProtocolId.java
+++ src/main/java/de/fhwedel/coinflip/protocol/model/id/ProtocolId.java
@@ -10,7 +10,8 @@ public enum ProtocolId {
       "Protocol version negotiation step two"), TWO(2, "Initialize SID negotiation"), THREE(3,
           "p and q generation"), FOUR(4, "Coin Encryption"), FIVE(5,
               "Coin flip - choose encrypted coin side"), SIX(6,
-                  "Coin flip - decrypt chosen coin side and initialize key exchange handshake");
+                  "Coin flip - decrypt chosen coin side and initialize key exchange handshake"), SEVEN(
+                      7, "Coin flip - answer key exchange handshake");
 
   private final int id;
   private final String message;
